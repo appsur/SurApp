aeb2761d98f3e192e0903d07f66caf90bef0c2fa
Grace Cao <gracecao@fb.com>
2018-07-16 14:57:48 -0700

    Added recycler view, created a friend item xml, and started the friend adapter.

--- app/src/main/java/com/pusheenicorn/safetyapp/FriendsActivity.java
+++ app/src/main/java/com/pusheenicorn/safetyapp/FriendsActivity.java
@@ -3,20 +3,34 @@ package com.pusheenicorn.safetyapp;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
+
+import com.pusheenicorn.safetyapp.models.User;
+
+import java.util.ArrayList;
 
 public class FriendsActivity extends AppCompatActivity {
     ProgressBar progressBar;
+    FriendsAdapter friendsAdapter;
+    ArrayList<User> friends;
+    RecyclerView rvFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
-        // get the Drawable custom_progressbar
-        Drawable draw = getDrawable(R.drawable.customprogressbar);
-        // set the drawable as progress drawable
-//        progressBar.setProgressDrawable(draw);
+        rvFriends = (RecyclerView) findViewById(R.id.rvFriendList);
+        friends = new ArrayList<>();
+        friendsAdapter = new FriendsAdapter(friends);
+
+        //setting up the Recycler View
+        rvFriends.setLayoutManager(new LinearLayoutManager(this));
+        //setting up the adapter
+        rvFriends.setAdapter(friendsAdapter);
+
 
 
     }
