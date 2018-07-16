de690f30a15131e60f30d6d008a67f8d5e957afc
jared1158 <jaredlns@gmail.com>
2018-07-16 11:34:04 -0700

    Merge branch 'master' of https://github.com/appsur/SurApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/pusheenicorn/safetyapp/ParseApp.java
@@ -0,0 +1,40 @@
+package com.pusheenicorn.safetyapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+/**
+ * Created by jared1158 on 7/16/18.
+ */
+
+public class ParseApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("sur-app")
+                .clientKey("pusheenicorn")
+                .server("http://sur-app.herokuapp.com/parse")
+                .build();
+        Parse.initialize(configuration);
+
+
+
+        // New test creation of object below
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
