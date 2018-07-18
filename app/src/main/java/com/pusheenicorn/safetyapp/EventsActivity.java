52a760733a1cea611e134f3eef0c778be736adca
Grace Cao <gracecao@fb.com>
2018-07-18 10:52:01 -0700

    Designed and added logo to home screen. Linked Events Activity to the Main Activity.

--- app/src/main/java/com/pusheenicorn/safetyapp/EventsActivity.java
+++ app/src/main/java/com/pusheenicorn/safetyapp/EventsActivity.java
@@ -2,14 +2,19 @@ package com.pusheenicorn.safetyapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class EventsActivity extends AppCompatActivity {
     ImageButton ibBanner;
+    BottomNavigationView bottomNavigationView;
     static final int REQUEST_CODE = 1;
     static final String TAG = "Success";
 
@@ -18,6 +23,30 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
         //initialize global variable
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_person:
+                        Intent goHome = new Intent(EventsActivity.this, MainActivity.class);
+                        Toast.makeText(EventsActivity.this, "Success!", Toast.LENGTH_LONG ).show();
+                        startActivity(goHome);
+                        return true;
+                    case R.id.action_message:
+                        // TODO -- link activities
+                        return true;
+                    case R.id.action_emergency:
+                        // TODO -- link activities
+                        return true;
+                    case R.id.action_friends:
+                        Toast.makeText(EventsActivity.this, "Already on Friends Page!", Toast.LENGTH_LONG ).show();
+                        return true;
+                }
+                return true;
+            }
+        });
+
         ibBanner = (ImageButton) findViewById(R.id.ibBanner);
         ibBanner.setOnClickListener(new View.OnClickListener() {
 
