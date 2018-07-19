4df3c559f74651d27a8b715617ccca9ace4dc490
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-19 16:08:23 -0700

    Fixed mistakesss.

--- app/src/main/java/com/pusheenicorn/safetyapp/MainActivity.java
+++ app/src/main/java/com/pusheenicorn/safetyapp/MainActivity.java
@@ -1,18 +1,13 @@
 package com.pusheenicorn.safetyapp;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
@@ -213,24 +208,22 @@ public class MainActivity extends AppCompatActivity {
     public void onCheckin(View view) {
         final Checkin checkin;
         final Date newCheckinDate;
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        //Location loc;
-        //double lat = loc.getLatitude()
-
-        LocationListener locationListener = new MyLocationListener();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-
+//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//        //Location loc;
+//        //double lat = loc.getLatitude()
+//
+//        LocationListener locationListener = new MyLocationListener();
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(
+//                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
 
         if (!isCheckedIn) {
             ibCheckin.setImageResource(R.drawable.check);
