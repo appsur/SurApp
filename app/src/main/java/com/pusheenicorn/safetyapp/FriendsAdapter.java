136cfa0675af0b8db66f2044aa1623f4d9d06127
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-23 11:42:35 -0700

    Populated friends and events list.

--- app/src/main/java/com/pusheenicorn/safetyapp/FriendsAdapter.java
+++ app/src/main/java/com/pusheenicorn/safetyapp/FriendsAdapter.java
@@ -11,14 +11,20 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.parse.FindCallback;
 import com.parse.ParseUser;
+import com.pusheenicorn.safetyapp.models.Checkin;
 import com.pusheenicorn.safetyapp.models.Friend;
 import com.pusheenicorn.safetyapp.models.User;
 
 import org.parceler.Parcels;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -57,12 +63,39 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
     //bind the values based on the position of the element
 
     @Override
-    public void onBindViewHolder(@NonNull FriendsAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final FriendsAdapter.ViewHolder holder, int position) {
         //get the data according to position
         Friend friend = mFriends.get(position);
         // Populate the item in the recycler view
         holder.tvName.setText(friend.getUser().getUsername());
-        holder.tvPhoneNumber.setText(friend.getUser().getPhonNumber());
+        String number = friend.getUser().getPhonNumber();
+        number = "(" + number.substring(0, 3) + ") " +
+                number.substring(3, 6) + "-" +
+                number.substring(6, 10);
+        holder.tvPhoneNumber.setText(number);
+
+        // Get the friend's last checkin id
+        final User friendUser = friend.getUser();
+        String checkinId = friendUser.getLastCheckin().getObjectId();
+
+        // Query by checkinId
+        final Checkin.Query checkinQuery = new Checkin.Query();
+        checkinQuery.getTop().whereEqualTo("objectId", checkinId);
+        checkinQuery.findInBackground(new FindCallback<Checkin>() {
+            @Override
+            public void done(List<Checkin> objects, com.parse.ParseException e) {
+                if (e == null) {
+                    // Get the checkin object and format its date
+                    final Checkin checkin = objects.get(0);
+                    Date date = checkin.getCreatedAt();
+                    String remaining = timeUntilCheckin(date, friendUser) + "";
+                    holder.tvTime.setText(remaining + "minutes");
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
     //get item count
@@ -76,6 +109,7 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.tvPhoneNumber) TextView tvPhoneNumber;
         @BindView(R.id.tvName) TextView tvName;
+        @BindView(R.id.tvTime) TextView tvTime;
 
         ParseUser user;
 
@@ -96,6 +130,36 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
                 //show activity
                 context.startActivity(i);
             }
+        }
+    }
+
+    public int timeUntilCheckin (Date prevDate, User user) {
+        // Define format type.
+        DateFormat df = new SimpleDateFormat("MM/dd/yy/HH/mm");
+
+        // Get current Date.
+        Date currDate = new Date();
+
+        // Split by regex "/" convert to int array and find time difference.
+        String[] currDateArr = df.format(currDate).split("/");
+        String[] prevDateArr = df.format(prevDate).split("/");
+        int[] currDateInts = new int[5];
+        int[] prevDateInts = new int[5];
+        for (int i = 0; i < 5; i++) {
+            currDateInts[i] = Integer.parseInt(currDateArr[i]);
+            prevDateInts[i] = Integer.parseInt(prevDateArr[i]);
+        }
+
+        int trueCurr = (currDateInts[0] * 43800) + (currDateInts[1] * 1440)
+                + (currDateInts[2] * 525600) + (currDateInts[3] * 60) + currDateInts[4];
+        int truePrev = (prevDateInts[0] * 43800) + (prevDateInts[1] * 1440)
+                + (prevDateInts[2] * 525600) + (prevDateInts[3] * 60) + prevDateInts[4];
+        int threshold = (int) user.getNumber("checkin");
+
+        if (trueCurr - truePrev > threshold) {
+            return 0;
+        } else {
+            return threshold - (trueCurr - truePrev);
         }
     }
 }
