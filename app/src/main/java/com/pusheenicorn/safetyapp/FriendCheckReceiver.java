8fea9f93f976405eee87e5c5adea4b6500cefb7c
jared1158 <jaredlns@gmail.com>
2018-07-31 17:05:38 -0700

    fried alert notifications implemented

---
+++ app/src/main/java/com/pusheenicorn/safetyapp/FriendCheckReceiver.java
@@ -0,0 +1,19 @@
+package com.pusheenicorn.safetyapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jared1158 on 7/31/18.
+ */
+
+public class FriendCheckReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context , Intent intent){
+
+    }
+
+
+}
