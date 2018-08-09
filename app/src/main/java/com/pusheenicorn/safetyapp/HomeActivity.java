08b2a42c890fe243df071cef4357f8f05cc4f0f0
Grace Cao <gracecao@fb.com>
2018-08-09 14:47:01 -0700

    Added comments to the home activity.

--- app/src/main/java/com/pusheenicorn/safetyapp/HomeActivity.java
+++ app/src/main/java/com/pusheenicorn/safetyapp/HomeActivity.java
@@ -21,19 +21,30 @@ public class HomeActivity extends AppCompatActivity {
         //initialized the parse user
         ParseUser currentUser = ParseUser.getCurrentUser();
         //makes sure that login or sign up occurs only if there is no current user
-
         if (currentUser != null) {
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         }
+        logInButton = findViewById(R.id.btnLogIn);
+        signUpButton = findViewById(R.id.btnSignUp);
     }
 
+    /**
+     * take user to the login activity
+     *
+     * @param view notify which object is clicked
+     */
     public void onLogInClick(View view) {
         //start intent to take user to the login activity class
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
     }
 
+    /**
+     * take user to the sign up activity
+     *
+     * @param view notify which object is clicked
+     */
     public void onSignUpClick(View view) {
         //start intent to take user to the sign up activity class
         Intent signUp = new Intent(this, SignupActivity.class);
